861731d33f35d00a723b1e46cc5c64d75d5f0fef
Vladislav-createpixel-Pavlov <83838004+Vladislav-createpixel-Pavlov@users.noreply.github.com>
2024-01-17 20:32:19 +0300

    Add files via upload

---
+++ ПЗ№3 Основы_Java(v1.2)/Тест Selenium/Test_BD_v1.2/src/main/java/managers/DriverManager.java
@@ -0,0 +1,48 @@
+package managers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class DriverManager {
+    /**
+     * Simple Singletone
+     */
+    private static DriverManager INSTANCE = null;
+
+    private DriverManager(){
+
+    }
+
+    public static DriverManager getDriverManager(){
+        if (INSTANCE == null) {
+            INSTANCE = new DriverManager();
+        }
+        return INSTANCE ;
+
+    }
+
+
+    /**
+     * Lazy Singletone
+     */
+    private WebDriver driver = null;
+
+    public WebDriver getDriver() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+    public void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+
+
+
+}
---
+++ ПЗ№3 Основы_Java(v1.2)/Тест Selenium/Test_BD_v1.2/src/test/java/tests/FirstTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import basetestclass.BaseTests;
+import managers.DriverManager;
+import org.junit.Test;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.concurrent.TimeUnit;
+
+
+//@RunWith(Parameterized.class)
+public class FirstTest extends BaseTests{
+   String value1 = "Kartofel_123)/.";
+
+    protected BaseTests app = new BaseTests();
+
+    @Test
+    @Tag("Овощ")
+    public void test_first() throws InterruptedException {
+        System.out.println("Параметры:\nТестовое значение №1 = "+ value1);
+        app.checkOpenPage();
+        app.Click("Добавить");
+        app.FillField("Наименование",value1);
+        app.Click("Овощ");
+        app.SaveClick();
+        app.CheckResult(value1);
+        System.out.println("Тест пройден");
+    }
+
+
+}
