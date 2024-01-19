cbafed6e8a01c2c9b1d3aee3b840616492e62131
Vladislav-createpixel-Pavlov <83838004+Vladislav-createpixel-Pavlov@users.noreply.github.com>
2024-01-19 23:35:31 +0300

    Add files via upload

---
+++ REST/Rest/src/test/java/tests/RestTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import Base.Base;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static io.opentelemetry.api.internal.ApiUsageLogger.log;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class RestTest extends Base {
+
+    @Test
+    void test1() {
+        Base.Insert();
+//        Base.Check();
+   }
+
+
+}
